494fb3772ed0ca928ccca19783048c55c00b1240
Marc Lohrer <Marc.Lohrer@gmx.de>
2018-08-28 20:53:08 +0200

    remove unused logic

--- StockTickR/StockCollector/Models/Stock.cs
+++ StockTickR/StockCollector/Models/Stock.cs
@@ -5,54 +5,6 @@ namespace StockDatabase.Models {
     public class Stock : BaseEntity<int> {
         public string Symbol { get; set; }
 
-        public decimal DayOpen { get; private set; }
-
-        public decimal DayLow { get; private set; }
-
-        public decimal DayHigh { get; private set; }
-
-        public decimal LastChange { get; private set; }
-
-        public decimal Change {
-            get {
-                return Price - DayOpen;
-            }
-        }
-
-        public double PercentChange {
-            get {
-                return (double) Math.Round (Change / Price, 4);
-            }
-        }
-        decimal _price;
-
-        public decimal Price {
-            get {
-                return _price;
-            }
-            set {
-                if (_price == value) {
-                    return;
-                }
-
-                LastChange = value - _price;
-                _price = value;
-
-                if (DayOpen == 0) {
-                    DayOpen = _price;
-                }
-                if (_price < DayLow || DayLow == 0) {
-                    DayLow = _price;
-                }
-                if (_price > DayHigh) {
-                    DayHigh = _price;
-                }
-            }
-        }
+        public decimal Price { get; set; }
     }
-
-    /*public class Symbol : BaseEntity
-    {
-        public string Name { get; set; }
-    }*/
 }
